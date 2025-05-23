ea7f462c6621a967bef222709834f01e02ec84a4
Moussa Dembele <134782079+Md123etplus@users.noreply.github.com>
2025-05-23 15:45:36 +0100

    merge: merged with feat/aaannn

--- database/migrations/2025_05_23_141155_remove_adress_from_annonces_table.php
+++ database/migrations/2025_05_23_141155_remove_adress_from_annonces_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('annonces', function (Blueprint $table) {
+        Schema::table('annonce', function (Blueprint $table) {
             $table->dropColumn('adress');
         });
     }
