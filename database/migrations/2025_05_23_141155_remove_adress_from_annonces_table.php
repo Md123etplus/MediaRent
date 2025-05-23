15f11180f472f4ea36bcfe1f2fda9796c40bcfb3
hajarbounab <bounab.hajar@etu.uae.ac.ma>
2025-05-23 15:26:15 +0100

    hhh

--- database/migrations/2025_05_23_141155_remove_adress_from_annonces_table.php
+++ database/migrations/2025_05_23_141155_remove_adress_from_annonces_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('annonces', function (Blueprint $table) {
-            //
+            $table->dropColumn('adress');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('annonces', function (Blueprint $table) {
-            //
+            $table->string('adress')->nullable();
         });
     }
 };
